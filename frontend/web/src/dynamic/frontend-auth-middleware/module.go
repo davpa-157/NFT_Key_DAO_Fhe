6efb93e212485fb9f428c37a4ca5f899b44ecb30
davpa-157 <sqiueed660@hotmail.com>
2025-12-05 04:00:41 +0800

    add service-components.py, module.go, EthereumConfig.dbg.json

---
+++ frontend/web/src/dynamic/frontend-auth-middleware/module.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\dynamic\frontend-auth-middleware\module.go
+// Generated at: 2025-10-19T12:32:08.598Z
---
+++ frontend/web/src/service-components.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\service-components.py
+// Generated at: 2025-10-19T12:32:08.599Z
